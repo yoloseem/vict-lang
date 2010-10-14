752dab45ac32483f9e8a4b8f267050ce569701a6
Hyunjun Kim <kim@hyunjun.kr>
2010-10-14 14:21:53 +0900

    Add Literals, Identifier, Array

---
+++ vict/parse.py
@@ -0,0 +1,77 @@
+r""":mod:`vict.parse`
+~~~~~~~~~~~~~~~~~~~~
+
+Identifiers::
+
+    >>> identifier.parse(u"xyz")
+    [u'xyz']
+
+Strings::
+
+    >>> string.parse(u'"test"')
+    [String(u'test')]
+    >>> string.parse(ur'"\"hi\""')
+    [String(u'"hi"')]
+
+Booleans::
+
+    >>> boolean.parse(u'True')
+    [Boolean(True)]
+    >>> boolean.parse(u'False')
+    [Boolean(False)]
+
+Integer::
+    
+    >>> integer.parse(u'1234')
+    [Integer(1234)]
+
+Array::
+
+    >>> array.parse(u"[]")
+    [Array()]
+    >>> array.parse(u'["abc"]')
+    [Array([String(u'abc')])]
+    >>> array.parse(u'["abc",]')
+    [Array([String(u'abc')])]
+    >>> array.parse(u'["abc",1]')
+    [Array([String(u'abc'), Integer(1)])]
+    >>> array.parse(u'["abc",1,]')
+    [Array([String(u'abc'), Integer(1)])]
+    >>> array.parse(u'[1,2,3]')
+    [Array([Integer(1), Integer(2), Integer(3)])]
+    >>> array.parse(u'[1,2,3,]')
+    [Array([Integer(1), Integer(2), Integer(3)])]
+    >>> array.parse(u'[1,2,3,4]')
+    [Array([Integer(1), Integer(2), Integer(3), Integer(4)])]
+
+
+"""
+from lepl import *
+import vict.tree
+
+
+with DroppedSpace():
+    identifier = Regexp(ur'[A-Za-z_<>=!@?$%^&*+-/][0-9A-Za-z_<>=!@?$%^&*+-/]*')
+
+    string = String() > vict.tree.String.parse
+    boolean = Literals(u'True', u'False') > vict.tree.Boolean.parse
+    integer = Integer() > vict.tree.Integer.parse
+    floating = Float()
+    none = Literal(u'None')
+    literal = string | boolean | integer | floating | none
+
+    expression = Delayed()
+    array = Literal(u"[") / (expression / u",")[:] / expression[:1] / u"]" > vict.tree.Array.parse
+
+    key = literal | identifier | (Literal(u'(') / expression / u')')
+    pair = key / u"::" / expression
+    dictionary = Literal(u"{") / (pair / u",")[:] / pair[:1] / u"}"
+
+    expression += (literal | array | dictionary | identifier) \
+                | ('(' / expression / ')')
+
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testmod()
+
---
+++ vict/tree.py
@@ -0,0 +1,72 @@
+""":mod:`vict.tree` --- Abstract syntax tree
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+class Expression(object):
+
+    @classmethod
+    def parse(cls, result):
+        return cls(result[0])
+
+
+class Literal(Expression):
+
+    pass
+
+class Integer(Literal):
+
+    def __init__(self, value):
+        self.value = int(value)
+
+    def __repr__(self):
+        return "Integer({0!r})".format(self.value)
+
+class Float(Literal):
+
+    pass
+
+class String(Literal, unicode):
+
+    def __repr__(self):
+        return "String({0})".format(unicode.__repr__(self))
+
+
+class Boolean(Literal):
+
+    __slots__ = "value",
+
+    @staticmethod
+    def parse(result):
+        return Boolean(result[0] == 'True')
+
+    def __init__(self, value):
+        self.value = bool(value)
+
+    def __repr__(self):
+        return "Boolean({0!r})".format(self.value)
+
+
+class None_(Literal):
+
+    pass
+
+
+class Array(Expression, tuple):
+
+    @staticmethod
+    def parse(result):
+        return Array(x for x in result if isinstance(x, Expression))
+
+    def __repr__(self):
+        if not self:
+            return "Array()"
+        endpos = -2 if len(self) == 1 else -1
+        return "Array([{0}])".format(tuple.__repr__(self)[1:endpos])
+
+Array()
+
+class Identifier(Expression):
+
+    pass
+
