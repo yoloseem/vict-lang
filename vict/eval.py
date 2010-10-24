96e4ecb7068a2d194a38978f9a8d1740d4e9a72b
Kim Hyunjun <kim@hyunjun.kr>
2010-10-24 17:49:24 +0900

    Add set proceed

---
+++ vict/eval.py
@@ -0,0 +1,33 @@
+""":mod:`vict.eval` --- Evaluate AST
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+"""
+
+import vict.parse
+import vict.tree
+import vict.env
+
+def evaluate(ast, env):
+
+    if type(ast) is list:
+        for item in ast:
+            evaluate(item, env)
+
+    if type(ast) is vict.tree.Program:
+        for line in ast.lines:
+            evaluate(line, env)
+
+    if type(ast) is vict.tree.Set:
+        if type(ast.left) is vict.tree.Identifier:
+            env.set_(ast.left.identifier, evaluate(ast.right, env))
+
+    if type(ast) is vict.tree.Integer:
+        return ast
+
+if __name__ == "__main__":
+    code = open("vict/test2.vict").read()
+    code = unicode(code.replace('\n', '').replace('\t', ' '))
+    ast = vict.parse.program.parse(code)
+    env = vict.env.built_in_env()
+    evaluate(ast, env)
+    print env.env
