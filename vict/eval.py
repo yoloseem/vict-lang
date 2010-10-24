27f1c87ee2ec4b2b002f593796f2e7ba622f78f3
Kim Hyunjun <kim@hyunjun.kr>
2010-10-24 20:24:19 +0900

    Add all literals handler

--- vict/eval.py
+++ vict/eval.py
@@ -17,16 +17,22 @@ def evaluate(ast, env):
         for line in ast.lines:
             evaluate(line, env)
 
+    if type(ast) is vict.tree.Identifier:
+        return env.get(ast.identifier)
+
     if type(ast) is vict.tree.Set:
         if type(ast.left) is vict.tree.Identifier:
             env.set_(ast.left.identifier, evaluate(ast.right, env))
 
-    if type(ast) is vict.tree.Integer:
+    literals = [vict.tree.Integer, vict.tree.Float, \
+                vict.tree.String, vict.tree.Boolean,
+                vict.tree.None_]
+    if type(ast) in literals:
         return ast
 
 if __name__ == "__main__":
     code = open("vict/test2.vict").read()
-    code = unicode(code.replace('\n', '').replace('\t', ' '))
+    code = unicode(code.replace('\t', ' '))
     ast = vict.parse.program.parse(code)
     env = vict.env.built_in_env()
     evaluate(ast, env)
